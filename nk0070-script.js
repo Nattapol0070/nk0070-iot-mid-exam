8b844ced1e3e4acb1b82a70bbb04a008d6b89e78
Nattapol0070 <s6301072510070@email.kmutnb.ac.th>
2022-10-03 22:09:06 +0700

    add submit form , read flow id show value

--- nk0070-script.js
+++ nk0070-script.js
@@ -1,9 +1,23 @@
 document.addEventListener('DOMContentLoaded',function (){
-    fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=flow-transducer&sensor_id=1')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const sensor_value = data.value;
-        document.querySelector('body').innerHTML = `Sensor value = ${sensor_value} `;
-    });
+      
+    document.querySelector('form').onsubmit = function (){
+        fetch('https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=flow-transducer')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const flow_sensor_id = document.querySelector('#flow_sensor_id').value;
+            let sensor_value = data[flow_sensor_id].value;
+            if(sensor_value !== undefined){
+                document.querySelector('#result').innerHTML = `Sensor value = ${sensor_value} `;
+            }
+            else{
+                document.querySelector('#result').innerHTML = "Invalid.";
+            }
+        })
+        .catch(error => {
+            console.log('Error:', error);
+        });
+
+        return false;
+    }
 });
