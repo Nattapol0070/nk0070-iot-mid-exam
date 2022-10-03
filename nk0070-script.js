ae3d1e13f9997e4019823ae5ae50e8ba40b56726
Nattapol0070 <s6301072510070@email.kmutnb.ac.th>
2022-10-03 23:40:21 +0700

    fix invalid sensor id

--- nk0070-script.js
+++ nk0070-script.js
@@ -6,13 +6,13 @@ document.addEventListener('DOMContentLoaded',function (){
         .then(data => {
             console.log(data);
             let flow_sensor_id = document.querySelector('#flow_sensor_id').value;
-            let sensor_name = data[flow_sensor_id].name;
-            let sensor_types = data[flow_sensor_id].type;
-            let sensor_unit = data[flow_sensor_id].unit;
-            let sensor_range = data[flow_sensor_id].range;
-            let sensor_plant = data[flow_sensor_id]["plant-site"];
-            let sensor_value = data[flow_sensor_id].value;
-            if(flow_sensor_id !== undefined){
+            let sensor_name = data[flow_sensor_id]?.name;
+            let sensor_types = data[flow_sensor_id]?.type;
+            let sensor_unit = data[flow_sensor_id]?.unit;
+            let sensor_range = data[flow_sensor_id]?.range;
+            let sensor_plant = data[flow_sensor_id]?.["plant-site"];
+            let sensor_value = data[flow_sensor_id]?.value;
+            if(data[flow_sensor_id] !== undefined){
                 document.querySelector('#flow-info').innerHTML = `**FLOW-Sensor-ID : ${flow_sensor_id}** 
                                                                         <br>Name : ${sensor_name}.</br> 
                                                                             Type : ${sensor_types}.</br>
