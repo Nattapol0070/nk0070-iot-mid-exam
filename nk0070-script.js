b4d8be1d37904a4fe02874d054e1f23525eed9f7
Nattapol0070 <s6301072510070@email.kmutnb.ac.th>
2022-10-07 05:22:16 +0700

    add bootstrap style

--- nk0070-script.js
+++ nk0070-script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded',function (){
 
-    //  ================================ SENSOR - TRANSDUCER ============================================
-    document.getElementById("sensor_form").onsubmit = function (){
+    //  ================================ GET - SENSOR - TRANSDUCER ============================================
+    document.getElementById("sensor_test").onsubmit = function (){
 
-        let sensor_select = document.querySelector('#sensor_select').value;
+        let sensor_select = document.querySelector('#sensor_select')?.value;
 
         fetch(`https://midterm-exam-010723313-2022.herokuapp.com/device?sensor_type=${sensor_select}`)
         .then(response => response.json())
         .then(data => {
-            console.log(data); // call data => inspect
-            let sensor_id = document.querySelector('#sensor_id').value; //select sensor id
+            console.log(data); // log data => inspect
+            let sensor_id = document.querySelector('#sensor_id:checked')?.value;
             let sensor_name = data[sensor_id]?.name; //sensor data location
             let sensor_types = data[sensor_id]?.type;
             let sensor_unit = data[sensor_id]?.unit;
@@ -17,13 +17,15 @@ document.addEventListener('DOMContentLoaded',function (){
             let sensor_plant = data[sensor_id]?.["plant-site"];
             let sensor_value = data[sensor_id]?.value;
             if(data[sensor_id] !== undefined){
-                document.querySelector('#sensor_info').innerHTML = `**Sensor-ID : ${sensor_id}** 
-                                                                        <br>Name : ${sensor_name}.</br> 
-                                                                            Type : ${sensor_types}.</br>
-                                                                            Unit : ${sensor_unit}.</br>
-                                                                            Range : ${sensor_range}.</br>
-                                                                            Plant-site : ${sensor_plant}.</br>
-                                                                            Value : ${sensor_value}</br> `; //show data on page
+                document.querySelector('#sensor_info').innerHTML = `<ul class="list-group">
+                                                                        <li class="list-group-item active" aria-current="true">Sensor-ID : ${sensor_id}.</li>
+                                                                        <li class="list-group-item">Name : ${sensor_name}.</li>
+                                                                        <li class="list-group-item">Type : ${sensor_types}.</li>
+                                                                        <li class="list-group-item">Unit : ${sensor_unit}.</li>
+                                                                        <li class="list-group-item">Range : ${sensor_range}.</li>
+                                                                        <li class="list-group-item">Plant-site : ${sensor_plant}.</li>
+                                                                        <li class="list-group-item">Value : ${sensor_value}</li>
+                                                                    </ul>`;
             }
             else{
                 document.querySelector('#sensor_info').innerHTML = "No-Data.";
@@ -35,18 +37,18 @@ document.addEventListener('DOMContentLoaded',function (){
 
         return false;
     }
-    //  ================================ SENSOR - TRANSDUCER ============================================
+    //  ================================ GET - SENSOR - TRANSDUCER ============================================
 
     //  ================================ POST - DATA ====================================================
     let PostFormData = document.getElementById('post_form')
     PostFormData.addEventListener('submit', function(e){
         e.preventDefault();
-        let st_id = document.getElementById('st_id').value;
-        let st_name = document.getElementById('st_name').value;
-        let sensor_name = document.getElementById('sensor_name').value;
-        let sensor_type = document.getElementById('sensor_type').value;
-        let sensor_unit = document.getElementById('sensor_unit').value;
-        let sensor_value = document.getElementById('sensor_value').value;
+        let st_id = document.getElementById("st_id").value;
+        let st_name = document.getElementById("st_name").value;
+        let sensor_name = document.getElementById("sensor_name").value;
+        let sensor_type = document.getElementById("sensor_type").value;
+        let sensor_unit = document.getElementById("sensor_unit").value;
+        let sensor_value = document.getElementById("sensor_value").value;
 
         fetch('https://midterm-exam-010723313-2022.herokuapp.com/student_post',{
             method: "POST",
@@ -66,4 +68,5 @@ document.addEventListener('DOMContentLoaded',function (){
         .catch(error => console.log(error));
     })
     //  ================================ POST - DATA =====================================================
+
 });
